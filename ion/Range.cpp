47d8bab2460529bdda618b1c7a08b92909a61680
Ryan Pearl <rpearl@endofunctor.org>
2012-04-16 14:17:31 -0400

    empty Range.cpp stub file

---
+++ ion/Range.cpp
@@ -0,0 +1,15 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=4 sw=4 et tw=79: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <stdio.h>
+
+#include "Ion.h"
+#include "MIR.h"
+#include "MIRGraph.h"
+
+using namespace js;
+using namespace js::ion;
+
