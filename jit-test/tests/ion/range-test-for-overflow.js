88f6ecdcab3d1d2e1138444bf42f4c6f380cabe0
Michael Sullivan <sully@msully.net>
2012-04-26 19:44:33 -0400

    Whitespace repair in a test.

--- jit-test/tests/ion/range-test-for-overflow.js
+++ jit-test/tests/ion/range-test-for-overflow.js
@@ -1,9 +1,9 @@
 function f() {
-	var i; 
-	for (i = 8; i <2147483647; i+=10);
+    var i;
+    for (i = 8; i <2147483647; i+=10);
     print(i);
 }
 
 for (var i = 0; i < 60; i++) {
-	f();
+    f();
 }
